33c06c90bfc1e7bfeacdc4eff73eef06ab77681b
Kobi Hari <kobi2294@yahoo.com>
2022-05-24 12:55:16 +0300

    Notepad example completed with lifecycle hooks demonstrated

--- Module 04/notepad/src/app/components/string-selector/string-selector.component.ts
+++ Module 04/notepad/src/app/components/string-selector/string-selector.component.ts
@@ -26,3 +26,34 @@ export class StringSelectorComponent implements OnInit {
   }
 
 }
+
+
+/*
+
+Events through the lifecycle of a component
+
+A. Lifetime begining - happens once
+---------------------
+1. The constructor is called
+2. The angular engine populates the inputs
+3. Angular engine calls a lifecycle hook called: "ngOnInit"
+4. The view is initialized
+5. The ViewChild properties are populated
+6. The ngAfterViewInit lifecycle hook is called
+
+B. Lifetime middle - happens more than once
+-------------------------------------------
+1. Angular sets value to one of the Inputs (or more)
+2. It then calls the ngOnChanges lifecycle hook and passes a SimpleChanges structure containing information
+   about the inputs that changed and their previous value, their current value and is it the first change to this 
+   property
+
+
+C. Lifetime conclusion - happens once
+-------------------------------------
+1. Angular calls the ngOnDestroy lifecycle hook
+2. It then destroys the component
+
+
+
+*/
--- Module 04/notepad/src/app/components/title/title.component.ts
+++ Module 04/notepad/src/app/components/title/title.component.ts
@@ -1,15 +1,49 @@
-import { Component, Input } from "@angular/core";
+import { AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from "@angular/core";
 
 @Component({
     selector: 'app-title', 
     templateUrl: './title.component.html', 
     styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
     @Input()
     caption: string = 'Title Caption';
 
+    private _color: string = 'Black';
+    
     @Input('caption-color')
-    color: string = 'Black';
+    get color(): string {return this._color}
+    set color(value: string) {
+        this._color = value; 
+        console.log('The color property was set to ' + value);
+    }
 
+
+    @ViewChild('titleElement')
+    myH1Element: any;
+
+
+    constructor() {
+        console.log(`app-title constructor, caption = ${this.caption}, myH1Element = ${this.myH1Element}`);
+    }
+
+    ngOnInit(): void {
+        console.log(`app-title ngOnInit, caption = ${this.caption}, myH1Element = ${this.myH1Element}`);        
+    }
+
+    ngAfterViewInit(): void {
+        console.log(`app-title ngAfterViewInit, caption = ${this.caption}, myH1Element = ${this.myH1Element}`);                
+        console.log(this.myH1Element);
+
+        this.myH1Element.nativeElement.title = 'Our amazing app, and this tooltip is set by code';
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('ngOnChanges');
+        console.log(changes);
+    }
+
+    ngOnDestroy(): void {
+        console.log('app-title ngOnDestroy');
+    }
 }
