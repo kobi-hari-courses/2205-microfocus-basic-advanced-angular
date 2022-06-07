f76d1f697e8a86067d851aa35c9a147256473bb7
Kobi Hari <kobi2294@yahoo.com>
2022-06-07 11:29:40 +0300

    application layout and routing is defined

--- Module 08/movies-app/src/app/app-routing.module.ts
+++ Module 08/movies-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, 
+  {path: 'home', component: HomeComponent}, 
+  {path: 'movies', component: MoviesListComponent}, 
+  {path: 'account', component: AccountComponent}, 
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Module 08/movies-app/src/app/app.module.ts
+++ Module 08/movies-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieEditComponent } from './components/movie-edit/movie-edit.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesListComponent,
+    MovieDetailsComponent,
+    MovieEditComponent,
+    NotFoundComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
