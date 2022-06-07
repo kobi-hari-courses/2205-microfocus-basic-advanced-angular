1cc86d47eea0d9590a5860097bbbc0a22b910cdf
Kobi Hari <kobi2294@yahoo.com>
2022-06-07 12:23:21 +0300

    movie details completedd

--- Module 08/movies-app/src/app/components/movie-details/movie-details.component.ts
+++ Module 08/movies-app/src/app/components/movie-details/movie-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Movie } from 'src/app/core/models/movie.model';
+import { MoviesService } from 'src/app/core/services/movies.service';
+import { map, switchAll } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
+  movie$!: Observable<Movie | undefined>;
 
-  constructor() { }
+  constructor(
+    private movies: MoviesService, 
+    private route: ActivatedRoute, 
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    let index$ = this.route.params.pipe(
+      map(prms => Number(prms['movieNumber']) )
+    );
+
+    this.movie$ = index$.pipe(
+      map(index => this.movies.getMovieByIndex(index)), 
+      switchAll()
+    )
+  }
+
+  async goNext() {
+    let index = Number(this.route.snapshot.params['movieNumber']);
+    this.router.navigate(['movies', index + 1]);
+  }
+
+  async goPrev() {
+    let index = Number(this.route.snapshot.params['movieNumber']);
+    this.router.navigate(['movies', index - 1]);
   }
 
 }
