2761e0b85ccd92cef72f5631497b946c8524616d
Kobi Hari <kobi2294@yahoo.com>
2022-08-04 13:05:11 +0300

    ng content concluded

--- Module 15/fun-with-content/src/app/components/expander/expander.component.ts
+++ Module 15/fun-with-content/src/app/components/expander/expander.component.ts
@@ -1,11 +1,17 @@
-import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, EventEmitter, Input, OnInit } from '@angular/core';
+import { ExpanderToggleDirective } from './expander-toggle.directive';
 
 @Component({
   selector: 'app-expander',
   templateUrl: './expander.component.html',
   styleUrls: ['./expander.component.css']
 })
-export class ExpanderComponent implements OnInit {
+export class ExpanderComponent implements OnInit, AfterContentInit {
+  // if the user supplied us with a toggle button, we use the toggle button for toggling
+  // but if the user did not supply a toggle button, we will toggle when the user clicks on the header
+
+  @ContentChild(ExpanderToggleDirective)
+  private toggleDirective: ExpanderToggleDirective | null = null;
 
   private _isExpanded: boolean = true;
   public get isExpanded(): boolean {
@@ -17,12 +23,28 @@ export class ExpanderComponent implements OnInit {
     this.toggled.emit(this._isExpanded);
   }
 
+  onHeaderClicked() {
+    console.log('header area clicked');
+    if (!this.toggleButtonExists) {
+      this.toggle();
+    }
+  }
+
   toggled = new EventEmitter<boolean>();
 
+  toggleButtonExists: boolean = false;
 
-  constructor() { }
+
+  constructor(
+    
+  ) {
+   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    this.toggleButtonExists = Boolean(this.toggleDirective);
   }
 
 }
